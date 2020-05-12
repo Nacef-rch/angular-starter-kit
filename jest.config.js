13855443de5763a8984bccaa4b92de9aa5537509
Nacef Racheh <rachehnacef@gmail.com>
2020-05-12 06:44:53 +0100

    conf: adding eslint+ prettier

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,27 @@
 module.exports = {
-  preset: "jest-preset-angular",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/Jest/setupJest.ts"],
-  moduleNameMapper: {
-    "^lodash-es$": "lodash",
-  },
-  globals: {
-    "ts-jest": {
-      tsConfig: "<rootDir>/tsconfig.spec.json",
-      diagnostics: true,
-      stringifyContentPathRegex: "\\.html$",
-      astTransformers: [
-        require.resolve("jest-preset-angular/InlineHtmlStripStylesTransformer"),
-      ],
+    preset: 'jest-preset-angular',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/Jest/setupJest.ts'],
+    moduleNameMapper: {
+        '^lodash-es$': 'lodash',
     },
-  },
-  testMatch: ["<rootDir>/**/**/*.spec.ts"],
-  collectCoverage: false,
-  collectCoverageFrom: [
-    "**/src/**/*.ts",
-    "!**/node_modules/**",
-    "!**/src/**/*.module.ts",
-    "!test/**",
-    "!**/polyfills.ts",
-    "!**/environments/**",
-    "!**/src/setupJest.ts",
-  ],
+    globals: {
+        'ts-jest': {
+            tsConfig: '<rootDir>/tsconfig.spec.json',
+            diagnostics: true,
+            stringifyContentPathRegex: '\\.html$',
+            astTransformers: [require.resolve('jest-preset-angular/InlineHtmlStripStylesTransformer')],
+        },
+    },
+    testMatch: ['<rootDir>/**/**/*.spec.ts'],
+    collectCoverage: false,
+    collectCoverageFrom: [
+        '**/src/**/*.ts',
+        '!**/node_modules/**',
+        '!**/src/**/*.module.ts',
+        '!test/**',
+        '!**/polyfills.ts',
+        '!**/environments/**',
+        '!**/src/setupJest.ts',
+    ],
 };
